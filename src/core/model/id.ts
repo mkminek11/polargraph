bf2612b35bbc4a823ac2975032105e639e3e1c88
mkminek11 <mkminek11@seznam.cz>
2026-07-22 14:54:26 +0200

    merge

--- src/core/model/id.ts
+++
@@ -1,5 +0,0 @@
-export function createId(prefix = 'id'): string {
-  const random = Math.random().toString(36).slice(2, 10);
-  const time = Date.now().toString(36);
-  return `${prefix}_${time}${random}`;
-}
